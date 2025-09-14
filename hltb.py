9aaeb212ebe240520c1e120e7b6db7165de8979e
OrginalS <36825453+OrginalS@users.noreply.github.com>
2025-09-14 17:58:53 +0000

    Update hltb.py
    colors

--- hltb.py
+++ hltb.py
@@ -12,18 +12,21 @@ def get_max_lengths(data):
 
 def print_dynamic_table(data, links):
 
+    GRAY = '\x1b[90m'
+    RESET = '\x1b[0m'
+
     # Calculate column widths
     max_lengths = get_max_lengths(data)
 
     # Print header
-    print("+" + "+".join("-" * (width + 2) for width in max_lengths) + "+")
+    print(GRAY + "+" + "+".join("-" * (width + 2) for width in max_lengths) + "+" + RESET)
 
     # Print header row
-    header = "| " + " | ".join(str(cell).ljust(width) for cell, width in zip(data[0], max_lengths)) + " |"
+    header = f"{GRAY}| {RESET}" + f"{GRAY} | {RESET}".join(str(cell).ljust(width) for cell, width in zip(data[0], max_lengths)) + f"{GRAY} |{RESET}"
     print(header)
 
     # Print separator
-    print("+" + "+".join("-" * (width + 2) for width in max_lengths) + "+")
+    print(GRAY + "+" + "+".join("-" * (width + 2) for width in max_lengths) + "+" + RESET)
 
     # Print data rows
     for idx, row in enumerate(data[1:], start=1):
@@ -31,14 +34,14 @@ def print_dynamic_table(data, links):
         first_col = f'\x1b]8;;{links[idx-1]}\x1b\\{row[0].ljust(max_lengths[0])}\x1b]8;;\x1b\\'
 
         # Format remaining columns normally
-        remaining_cols = " | ".join(str(cell).ljust(width) for cell, width in zip(row[1:], max_lengths[1:]))
+        remaining_cols = f"{GRAY} | {RESET}".join(str(cell).ljust(width) for cell, width in zip(row[1:], max_lengths[1:]))
 
         # Combine first column with remaining columns
-        formatted_row = f"| {first_col} | {remaining_cols} |"
+        formatted_row = f"{GRAY}|{RESET} {first_col} {GRAY}|{RESET} {remaining_cols} {GRAY}|{RESET}"
         print(formatted_row)
 
     # Print bottom border
-    print("+" + "+".join("-" * (width + 2) for width in max_lengths) + "+")
+    print(GRAY + "+" + "+".join("-" * (width + 2) for width in max_lengths) + "+" + RESET)
 
 # Main program
 search_term = " ".join(sys.argv[1:])
